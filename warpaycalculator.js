625b1c891fec13dfb8911750439120909f0f45e6
Shain Collins <scolli03.sc@gmail.com>
2024-08-16 23:38:12 -0500

    test

--- warpaycalculator.js
+++ warpaycalculator.js
@@ -1,7 +1,7 @@
 // ==UserScript== 
 // @name         War Payment Calculator
 // @namespace    http://tampermonkey.net/
-// @version      2.0.0.1
+// @version      2.1.0.0
 // @description  try to take over the world!
 // @author       Scolli03 [3150751]
 // @match        https://www.torn.com/war.php?step=rankreport&rankID=*
